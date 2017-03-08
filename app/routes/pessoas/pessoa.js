181bafdaa7a0093b573ad32cff6d454e1d2cca3a
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2017-03-07 22:39:44 -0300

    Mostrando pessoa

--- app/routes/pessoas/pessoa.js
+++ app/routes/pessoas/pessoa.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    pessoas: [{id: "1", nome: 'Clairton'}, {id:"2", nome: 'Rodrigo'}],
+    
+    
+
+    model(params){
+        return this.get('pessoas').findBy('id', params.id);
+    }
 });
