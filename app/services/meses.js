a3bb3b06d688678776cfa39b2cb9171c502f8e65
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2017-03-12 19:22:17 -0300

    Messe multiidioma

--- app/services/meses.js
+++ app/services/meses.js
@@ -1,22 +1,40 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
-  meses: [
-    'Janeiro', 
-    'Fevereiro', 
-    'Março', 
-    'Abril',
-    'Maio', 
-    'Junho', 
-    'Julho', 
-    'Agosto',
-    'Setembro', 
-    'Outubro', 
-    'Novembro', 
-    'Dezembro'
-  ],
+  idioma: 'pt-BR',
+  meses: {
+    'pt-BR': [
+      'Janeiro', 
+      'Fevereiro', 
+      'Março', 
+      'Maio', 
+      'Junho', 
+      'Julho', 
+      'Setembro', 
+      'Outubro', 
+      'Novembro', 
+      'Dezembro'
+    ],
+    'en-US': [
+      'January', 
+      'February', 
+      'March', 
+      'May', 
+      'June', 
+      'July', 
+      'September', 
+      'October', 
+      'November', 
+      'December'
+    ]
+  },
 
   getPorExtenso(ordinal){
-    return this.get('meses')[ordinal];
+    let idioma = this.get('idioma');
+    let meses = this.get('meses')[idioma];
+    if(!meses){
+        meses = this.get('meses')['pt-BR'];
+    }
+    return meses[ordinal];
   }
 });
