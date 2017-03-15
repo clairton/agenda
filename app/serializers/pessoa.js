a8cc236fdfdc643e5467258106d76d992cf06e90
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2017-03-14 21:09:01 -0300

    Pessoa serializer extendendo de DS.EmbeddedRecordsMixin

--- app/serializers/pessoa.js
+++ app/serializers/pessoa.js
@@ -1,6 +1,7 @@
+import DS from 'ember-data';
 import ApplicationSerializer from './application';
 
-export default ApplicationSerializer.extend({
+export default ApplicationSerializer.extend(DS.EmbeddedRecordsMixin,{
   attrs: {
     telefones: {
       serialize: 'records',
