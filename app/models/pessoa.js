6473fca32a2e6633fdb1b3867e310ad67aaaa628
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2017-03-30 22:52:57 -0300

    Utilizando o ember-moment-transform

--- app/models/pessoa.js
+++ app/models/pessoa.js
@@ -1,8 +1,10 @@
+//app/models/pessoa.js
+
 import DS from 'ember-data';
 
 export default DS.Model.extend({
   nome: DS.attr('string'),
   sobrenome: DS.attr('string'),
-  nascidoEm: DS.attr('date'),
+  nascidoEm: DS.attr('moment', {format: 'DD/MM/YYYY'}),
   telefones: DS.hasMany('telefone', {async: true})
 });
