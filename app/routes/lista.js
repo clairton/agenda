4bc158181b584f566291b6e55d6e3563e55945e5
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2017-05-02 21:00:56 -0300

    Lista no /

--- app/routes/lista.js
+++ app/routes/lista.js
@@ -1,25 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    pessoas: [{id: "1", nome: 'Clairton'}, {id:"2", nome: 'Rodrigo'}],
- 
+
   queryParams: {
     nome: {
       refreshModel: true
     }
   },
 
-    model(params){
-        let filter = {};
-        if(!Ember.isEmpty(params.nome)){
-          filter.nome = params.nome;
-        }
-        return this.get('store').query('pessoa', filter);
-    },
-    
-    actions:{
-        nova(){
-            this.transitionTo('nova');
-        }
+  model(params){
+    let filter = {};
+    if(!Ember.isEmpty(params.nome)){
+      filter.nome = params.nome;
     }
+    return this.get('store').query('pessoa', filter);
+  },
+  
+  actions:{
+    nova(){
+        this.transitionTo('nova');
+    }
+  }
 });
